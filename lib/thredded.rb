b63ac813f7fb5df316b7d545809b864664c30078
Joel Oliveira <joel@thoughtbot.com>
2013-05-01 10:39:22 -0400

    Create a fake user in the dummy app
    This dummy app's User model is an in-theory stand-in for what will, in practice, be the engine users' own user models. Those user models will more often than not be called "User", but why assume?
    
    The mattr_accessor in the Thredded module allows us to configure this user class from the context of the parent application.
    
    For example, in your parent application's `config/initializers/thredded.rb`:
    
    ```
    # Thredded.user_class = 'User'
    Thredded.user_class = 'MyUser'
    ```

--- lib/thredded.rb
+++ lib/thredded.rb
@@ -13,4 +13,19 @@ require 'thredded/filter/syntax'
 require 'thredded/filter/textile'
 
 module Thredded
+  mattr_accessor :user_class
+
+  def self.user_class
+    if @@user_class.is_a?(Class)
+      raise 'Please use a string instead of a class'
+    end
+
+    if @@user_class.is_a?(String)
+      begin
+        Object.const_get(@@user_class)
+      rescue NameError
+        @@user_class.constantize
+      end
+    end
+  end
 end
---
+++ spec/dummy/app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+end
