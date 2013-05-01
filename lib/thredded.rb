0d0cf7c848d0deb579a5b123a6995641c88f0d9d
Joel Oliveira <joel@thoughtbot.com>
2013-05-01 09:57:55 -0400

    Require our dependencies explicitly since the gemspec won't

--- lib/thredded.rb
+++ lib/thredded.rb
@@ -1,4 +1,16 @@
 require 'thredded/engine'
+require 'cancan'
+require 'carrierwave'
+require 'kaminari'
+require 'friendly_id'
+require 'thredded/filter/base'
+require 'thredded/filter/at_notification'
+require 'thredded/filter/attachment'
+require 'thredded/filter/bbcode'
+require 'thredded/filter/emoji'
+require 'thredded/filter/markdown'
+require 'thredded/filter/syntax'
+require 'thredded/filter/textile'
 
 module Thredded
 end
