34c030cdf75e8db7f676be2d8a8b505d099a919f
Joel Oliveira <joel@thoughtbot.com>
2013-05-19 15:02:28 -0400

    [JRO] require dependency in TopicObserver
    topic observer needed to be scoped in the thredded module and require
    the private_topic_notifier

--- app/models/thredded/topic_observer.rb
+++ app/models/thredded/topic_observer.rb
@@ -1,5 +1,9 @@
-class TopicObserver < ActiveRecord::Observer
-  def after_save(topic)
-    PrivateTopicNotifier.new(topic).notifications_for_private_topic
+require 'thredded/private_topic_notifier'
+
+module Thredded
+  class TopicObserver < ActiveRecord::Observer
+    def after_save(topic)
+      PrivateTopicNotifier.new(topic).notifications_for_private_topic
+    end
   end
 end
