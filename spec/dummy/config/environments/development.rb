bded2a59190f6c45998538f7cf391af174ac7727
Joel Oliveira <joel@thoughtbot.com>
2013-06-09 15:21:59 -0400

    [JRO] Reload engine files in dummy
    By default the engine doesn't get loaded on every request.
    
    `config.reload_plugins = true` in the dev environment's config will allow for reloading on requests like in normal dev mode.

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -10,7 +10,7 @@ Dummy::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -34,4 +34,6 @@ Dummy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.reload_plugins = true
 end
