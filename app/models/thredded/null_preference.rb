46baed4313d5301ea6a6821bf6e2b24a32ee4b29
Joel Oliveira <joel@thoughtbot.com>
2013-10-19 16:55:38 -0400

    [JRO] NullPreference needs namespacing
    Wrap NullPreference class with Thredded module

--- app/models/thredded/null_preference.rb
+++ app/models/thredded/null_preference.rb
@@ -1,9 +1,11 @@
-class NullPreference
-  def notify_on_mention
-    true
-  end
+module Thredded
+  class NullPreference
+    def notify_on_mention
+      true
+    end
 
-  def notify_on_message
-    true
+    def notify_on_message
+      true
+    end
   end
 end
