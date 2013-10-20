8e3bb3e317dd02ddabf0259d2c7dd90479a5aaa0
Joel Oliveira <joel@thoughtbot.com>
2013-10-19 21:12:14 -0400

    [JRO] User has many messageboards
    . . .  through their roles.
    
    Remove User.recently_active_in as it's not being used.

--- app/models/thredded/user_extender.rb
+++ app/models/thredded/user_extender.rb
@@ -12,17 +12,10 @@ module Thredded
       has_many :thredded_roles, class_name: 'Thredded::Role', foreign_key: 'user_id'
       has_many :thredded_topics, class_name: 'Thredded::Topic', foreign_key: 'user_id'
       has_many :thredded_read_topics, class_name: 'Thredded::UserTopicRead', foreign_key: 'user_id'
+      has_many :thredded_messageboards, through: :thredded_roles, class_name: 'Thredded::Messageboard', source: :messageboard
 
       has_one :thredded_user_detail, class_name: 'Thredded::UserDetail', foreign_key: 'user_id'
       has_one :thredded_user_preference, class_name: 'Thredded::UserPreference', foreign_key: 'user_id'
-
-
-      def self.recently_active_in(messageboard)
-        joins(:thredded_roles)
-          .where(thredded_roles: { messageboard_id: messageboard.id })
-          .where('thredded_roles.last_seen > ?', 5.minutes.ago)
-          .order('thredded_roles.last_seen')
-      end
     end
   end
 end
