447cf2b6b2cddc56e8d8639aca7ffe75e63b5162
Joel Oliveira <joel.oliveira@gmail.com>
2013-11-06 16:05:12 -0500

    [JRO] Remove rspec/autorun from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment', __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'shoulda-matchers'
