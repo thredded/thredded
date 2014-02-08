154eda23b6b6fff048367f2f140e70f5d7bb48f2
Taylor Kearns <tkearns@gmail.com>
2014-02-08 08:31:18 -0500

    Tidy up initial spec

--- spec/features/thredded/user_stars_a_topic_spec.rb
+++ spec/features/thredded/user_stars_a_topic_spec.rb
@@ -1,60 +1,53 @@
 require 'spec_helper'
 
 feature 'User starring a topic' do
-  before do
-    pending 'Initial specs for review'
-
-    @topics = three_topics
-  end
-
   scenario 'sees a list of unstarred topics' do
-    @topics.visit_index
+    topics = three_topics
+    topics.visit_index
 
-    expect(@topics).to have(0).starred_topics
+    expect(topics).to have(0).starred_topics
   end
 
   scenario 'views an unstarred topic' do
-    @topics.visit_latest_topic
+    topics = three_topics
+    topic = topics.visit_latest_topic
 
-    expect(page).to have_link('Star this topic')
-
-    expect(page).to_not have_stars
+    expect(topic).to be_able_to_star
+    expect(topic).to be_rated_as(0)
   end
 
   scenario 'stars a topic' do
-    @topics.visit_latest_topic
+    topics = three_topics
+    topic = topics.visit_latest_topic
+    topic.star_topic
 
-    click_on 'Star this topic'
+    expect(topic).to be_rated_as(1)
+    expect(topic).to be_able_to_unstar
 
-    expect(page).to have_content('Star rating: 1')
-    expect(page).to have_link('Unstar this topic')
+    topics.visit_index
 
-    expect(page).to_not have_link('Star this topic')
-
-    @topics.visit_index
-
-    expect(@topics).to have(1).starred_topics
+    expect(topics).to have(1).starred_topics
   end
 
   scenario 'unstars a topic' do
-    @topics.visit_latest_topic
+    topics = three_topics
+    topics.visit_latest_topic
+    topic.star_topic
+    topic.unstar_topic
 
-    click_on 'Star this topic'
+    expect(topic).to be_able_to_star
+    expect(topic).to be_rated_as(0)
 
-    click_on 'Unstar this topic'
+    topics.visit_index
 
-    expect(page).to have_link('Star this topic')
-
-    expect(page).to_not have_stars
-
-    @topics.visit_index
-
-    expect(@topics).to have(0).starred_topics
+    expect(topics).to have(0).starred_topics
   end
 
   def three_topics
-    messageboard = create(:messageboard)
-    create_list(:topic, 3, messageboard: messageboard)
-    PageObject::Topics.new(messageboard)
+    @three_topics ||= begin
+      messageboard = create(:messageboard)
+      create_list(:topic, 3, messageboard: messageboard)
+      PageObject::Topics.new(messageboard)
+    end
   end
 end
