a14ba7822d4a120f74fb1e6f4467c8195e313a31
Joel Oliveira <joel.oliveira@gmail.com>
2014-06-25 10:19:01 -0400

    [JRO] Failing private topic integration specs

--- spec/features/thredded/user_views_private_topics_spec.rb
+++ spec/features/thredded/user_views_private_topics_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'User viewing topics' do
+feature 'User viewing private topics' do
   before do
     PageObject::User.new(user).log_in
   end
@@ -10,6 +10,40 @@ feature 'User viewing topics' do
     private_topics.visit_index
 
     expect(private_topics).to have(1).private_topic
+  end
+
+  scenario 'reads a private topic and it is marked as read' do
+    private_topics = one_private_topic
+    private_topics.visit_index
+
+    expect(private_topics).to have(1).private_topic
+    expect(private_topics).to have(1).unread_private_topic
+
+    private_topics.view_private_topic
+    private_topics.visit_index
+
+    expect(private_topics).to have(0).unread_private_topic
+  end
+
+  scenario 'sees that an old topic has been updated' do
+    private_topics = one_private_topic
+    private_topics.visit_index
+    expect(private_topics).to have(1).private_topic
+    expect(private_topics).to have(0).unread_private_topic
+
+    private_topics.update_all_private_topics
+    private_topics.visit_index
+
+    expect(private_topics).to have(1).unread_private_topic
+  end
+
+  scenario 'is notified in the navigation that there are unread topics' do
+    navigation = app_navigation
+    private_topics = one_private_topic
+    private_topics.visit_index
+
+    expect(navigation).to have_unread_private_topics
+    expect(private_topics).to have(1).unread_private_topic
   end
 
   def user
