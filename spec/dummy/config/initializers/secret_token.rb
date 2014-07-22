b61c709d7fc127de80d8dfd03aef6fa24e7bb0ad
Joel Oliveira <joel.oliveira@gmail.com>
2014-07-22 19:18:17 -0400

    [JRO] Add `secret_key_base` to quell the warning
    Rails 4 noise

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '80d371f907c54c965d525303daef0e356b9ae418e8a0b0a5d3185336cf1465ca8484c98bf66d756e6828ef883f8f3c68cffbda007eb3c66e4729468b5b5db4cb'
+Dummy::Application.config.secret_key_base = '49ade6ce637f129a386a7a4c662ba034dd49a8f3741f72ddc6dccf77d6171e33939d54489d92372177215b1f2a8e75216e8a732d250b0ca08f56172635db0bfd'
