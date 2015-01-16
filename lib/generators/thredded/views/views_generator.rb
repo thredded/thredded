cfde9e56c2dd5596ce048776ee03022605fc286e
Joel Oliveira <joel.oliveira@gmail.com>
2015-01-15 23:07:15 -0500

    [JRO] Copy views to parent app w/generator

--- lib/generators/thredded/views/views_generator.rb
+++ lib/generators/thredded/views/views_generator.rb
@@ -1,8 +1,11 @@
 module Thredded
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      desc "Thredded views"
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../../../../app/views', __FILE__)
+
+      def copy_view_files
+        directory 'thredded', 'app/views/thredded'
+      end
     end
   end
 end
