d0d513338bcd1f8e57b1a9544b676d542997d2b6
Joel Oliveira <joel.oliveira@gmail.com>
2015-04-13 11:03:55 -0400

    [JRO] Update README and set default theme
    The theme config is not set by default - this commit ensures that it is.

--- lib/thredded.rb
+++ lib/thredded.rb
@@ -46,6 +46,7 @@ module Thredded
   self.queue_memory_log_level = Logger::WARN
   self.queue_inline = false
   self.email_reply_to = -> postable { "#{postable.hash_id}@#{Thredded.email_incoming_host}" }
+  self.theme = :default
 
   def self.user_class
     if @@user_class.is_a?(Class)
