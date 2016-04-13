4bef74ff74a35d0e8407ffb7723a5bfe12f5a918
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-04-13 01:09:42 +0100

    i18n messageboard meta and byline
    Also moves the rendering bits to the view, and the data bits to the
    model, out of the decorator.

--- app/decorators/thredded/messageboard_decorator.rb
+++ app/decorators/thredded/messageboard_decorator.rb
@@ -1,8 +1,5 @@
 module Thredded
   class MessageboardDecorator < SimpleDelegator
-    include ActionView::Helpers::NumberHelper
-    include ActionView::Helpers::TagHelper
-
     def initialize(messageboard)
       super
       @messageboard = messageboard
@@ -10,24 +7,6 @@ module Thredded
 
     def original
       messageboard
-    end
-
-    def meta
-      topics_count = number_to_human(messageboard.topics_count)
-      posts_count = number_to_human(messageboard.posts_count)
-
-      "#{topics_count} topics / #{posts_count} posts".downcase
-    end
-
-    def latest_topic
-      @latest_topic ||= begin
-        messageboard.topics.order_recently_updated_first.first ||
-          Thredded::NullTopic.new
-      end
-    end
-
-    def latest_user
-      latest_topic.last_user
     end
 
     def category_options
--- app/models/thredded/messageboard.rb
+++ app/models/thredded/messageboard.rb
@@ -17,8 +17,10 @@ module Thredded
     has_many :user_messageboard_preferences, dependent: :destroy
     has_many :posts, dependent: :destroy
     has_many :topics, dependent: :destroy, inverse_of: :messageboard
-    has_many :user_details, through: :posts
+    has_one :latest_topic, -> { order_recently_updated_first },
+            class_name: 'Thredded::Topic'
 
+    has_many :user_details, through: :posts
     has_many :messageboard_users,
              inverse_of:  :messageboard,
              foreign_key: :thredded_messageboard_id
@@ -42,6 +44,10 @@ module Thredded
 
     def decorate
       MessageboardDecorator.new(self)
+    end
+
+    def latest_user
+      latest_topic.last_user
     end
 
     def slug_candidates
