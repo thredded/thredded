386c6d91749fc3a4cbec76e264a6f2c5fd8fd093
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-04-15 00:24:16 +0100

    Dummy app redirect back on login if back not login

--- spec/dummy/app/controllers/sessions_controller.rb
+++ spec/dummy/app/controllers/sessions_controller.rb
@@ -6,7 +6,11 @@ class SessionsController < ApplicationController
   def create
     session[:user_id] = Thredded.user_class.where(name: params[:name])
       .first_or_initialize.tap { |user| user.update!(admin: params[:admin]) }.id
-    redirect_back fallback_location: root_path
+    if request.referer != new_session_url
+      redirect_back fallback_location: root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
