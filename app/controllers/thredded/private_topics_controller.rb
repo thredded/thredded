5739f50a513377392205a41958b5a2d3ca25dc10
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-04-14 23:27:00 +0100

    Require login for PrivateTopicsController

--- app/controllers/thredded/private_topics_controller.rb
+++ app/controllers/thredded/private_topics_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 module Thredded
   class PrivateTopicsController < Thredded::ApplicationController
+    before_action :thredded_require_login!
     helper_method :private_topic
 
     def index
