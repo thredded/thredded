ed9935dd31ae5a624ffcf1ab3eaeaeb285d86006
Joel Oliveira <joel.oliveira@gmail.com>
2016-04-15 17:42:20 -0400

    Update per deprecation warnings

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -10,7 +10,7 @@ Dummy::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -32,7 +32,7 @@ Dummy::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
