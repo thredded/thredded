3de172cbafe6ad1d662d0f6d2b55faa11d3fb8bf
Ramiro Varandas Jr <ramirovjnr@gmail.com>
2016-04-27 21:55:32 -0300

    Creating translation for pt-BR language refs #62
    - Cloned the en.yml file and translated all available keys
    - Added pt-BR key to the available_locales at application.rb for testing

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -43,7 +43,7 @@ module Dummy
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
 
-    config.i18n.available_locales = [:en, :es]
+    config.i18n.available_locales = [:en, :es, :'pt-BR']
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
