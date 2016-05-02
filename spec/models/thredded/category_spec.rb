3796775581e1ed58f813800028b9cad1129fa825
Vincent Auriac <vincent.auriac@gmail>
2016-05-02 11:06:43 +0700

    remove specification about a behaviour belonging to another model (topic)

--- spec/models/thredded/category_spec.rb
+++ spec/models/thredded/category_spec.rb
@@ -3,20 +3,5 @@ require 'spec_helper'
 
 module Thredded
   describe Category do
-    it 'should allow no categories' do
-      topic = create(:topic)
-      topic.category_ids = nil
-      topic.save
-
-      expect(topic).to be_valid
-    end
-
-    it 'should allow a category' do
-      topic = create(:topic)
-      topic.categories << create(:category, messageboard: topic.messageboard)
-      topic.save
-
-      expect(topic.categories).not_to be_nil
-    end
   end
 end
