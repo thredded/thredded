a3c0928f2d547c220df742dec84aab4193d28f2c
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-05-02 16:17:09 +0300

    I18n for rails-timeago
    Refs #270 #62 #264

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require thredded
+//= require locales/jquery.timeago.pt-br
 //= require_tree ./app
 // Must be required last:
 //= require turbolinks
