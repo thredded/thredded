b9e432f6810d53b53d34612e367c13e992f2efa5
Tim Diggins <tim@red56.co.uk>
2016-06-01 17:55:20 +0100

    explicitly show dynamic i18n lookups so no need for loose checking

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -4,7 +4,7 @@ require 'i18n/tasks'
 RSpec.describe 'I18n' do
   let(:i18n) { I18n::Tasks::BaseTask.new }
   let(:missing_keys) { i18n.missing_keys }
-  let(:unused_keys) { i18n.unused_keys(strict: false) }
+  let(:unused_keys) { i18n.unused_keys }
 
   it 'does not have missing keys' do
     expect(missing_keys).to be_empty,
