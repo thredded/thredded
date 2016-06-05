7808463b4941fc99bb69804e8e8333f125b7598a
Tim Diggins <tim@red56.co.uk>
2016-06-05 07:41:23 +0100

    Extract utility method for clarity

--- app/controllers/thredded/topics_controller.rb
+++ app/controllers/thredded/topics_controller.rb
@@ -74,7 +74,7 @@ module Thredded
       @new_topic = TopicForm.new(new_topic_params)
       authorize_creating @new_topic.topic
       if @new_topic.save
-        UserTopicReadState.create!(user: thredded_current_user, postable: @new_topic.topic, read_at: Time.zone.now, page: 1) if signed_in?
+        UserTopicReadState.read_on_first_post!(thredded_current_user, @new_topic.topic) if signed_in?
 
         redirect_to messageboard_topics_path(messageboard)
       else
