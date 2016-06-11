1ab4cb571b22c11bd468f26f657738cff02e6b77
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-06-11 14:51:47 +0100

    rubocop

--- spec/dummy/config/initializers/rack_profiler.rb
+++ spec/dummy/config/initializers/rack_profiler.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 if Rails.env.development?
   require 'rack-mini-profiler'
   Rack::MiniProfilerRails.initialize!(Rails.application)
   Rack::MiniProfiler.config.tap do |c|
     c.backtrace_remove = Thredded::Engine.root.to_s
-    c.backtrace_includes = [/^\/?(app|config|lib|spec)/]
+    c.backtrace_includes = [%r{^/?(app|config|lib|spec)}]
   end
 end
