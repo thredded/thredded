15c3b13caafbfe34261e0044cf9a67ae85aa4b53
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-06-12 21:43:17 +0100

    Add missing class_name to UserTopicFollow#user

--- app/models/thredded/user_topic_follow.rb
+++ app/models/thredded/user_topic_follow.rb
@@ -3,7 +3,7 @@ module Thredded
   class UserTopicFollow < ActiveRecord::Base
     enum reason: [:manual, :posted, :mentioned]
 
-    belongs_to :user, inverse_of: :thredded_topic_follows
+    belongs_to :user, inverse_of: :thredded_topic_follows, class_name: Thredded.user_class
     belongs_to :topic, inverse_of: :user_follows
 
     validates :user_id, presence: true
