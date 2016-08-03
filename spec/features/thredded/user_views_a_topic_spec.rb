24109c945346f54e5681e80df4f579b8040406ae
EllaNancyFay <ella@red56.uk>
2016-08-03 11:06:20 +0100

    displays that no one is following for unfollowed topic

--- spec/features/thredded/user_views_a_topic_spec.rb
+++ spec/features/thredded/user_views_a_topic_spec.rb
@@ -4,16 +4,33 @@ require 'spec_helper'
 feature 'User views a topic' do
   let(:user) { create(:user) }
   let(:messageboard) { create(:messageboard) }
-  let(:a_followed_topic) do
-    topic = create(:topic, with_posts: 1, messageboard: messageboard)
-    Thredded::UserTopicFollow.create_unless_exists(user.id, topic.id)
-    PageObject::Topic.new(topic)
+
+  context 'for a followed topic' do
+    let(:a_followed_topic) do
+      topic = create(:topic, with_posts: 1, messageboard: messageboard)
+      Thredded::UserTopicFollow.create_unless_exists(user.id, topic.id)
+      PageObject::Topic.new(topic)
+    end
+
+    scenario 'can see list of users following topic' do
+      a_followed_topic.visit_topic
+      within '.thredded--topic-header' do
+        expect(page).to have_content(user.name)
+      end
+    end
   end
 
-  scenario 'can see list of users following topic' do
-    a_followed_topic.visit_topic
-    within '.thredded--topic-header' do
-      expect(page).to have_content(user.name)
+  context 'for an unfollowed topic' do
+    let(:a_unfollowed_topic) do
+      topic = create(:topic, messageboard: messageboard)
+      PageObject::Topic.new(topic)
+    end
+
+    scenario 'can see that no one is following' do
+      a_unfollowed_topic.visit_topic
+      within '.thredded--topic-header' do
+        expect(page).to have_content('No one is following this topic')
+      end
     end
   end
 end
