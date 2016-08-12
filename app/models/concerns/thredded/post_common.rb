a5e6464f3a0cb69f54e8a42abf371c342fd2e63e
Tim Diggins <tim@red56.co.uk>
2016-08-12 08:46:41 +0100

    stable ordering for date clashes

--- app/models/concerns/thredded/post_common.rb
+++ app/models/concerns/thredded/post_common.rb
@@ -13,8 +13,8 @@ module Thredded
 
       validates :content, presence: true
 
-      scope :order_oldest_first, -> { order(created_at: :asc) }
-      scope :order_newest_first, -> { order(created_at: :desc) }
+      scope :order_oldest_first, -> { order(created_at: :asc, id: :asc) }
+      scope :order_newest_first, -> { order(created_at: :desc, id: :desc) }
     end
 
     def avatar_url
