409738d6ae89b2a9a180bcdf3b0793ff3e74e8c8
Tim Diggins <tim@red56.uk>
2016-09-04 19:26:30 +0100

    rename dummy's current_user to make specs trap misuse of it. (#393)
    It would be nice to prevent mistaken uses of current_user, when
    we should be using thredded_current_user (see #391) - so maybe
    we can have the spec smoke it out by having the dummy use a differently
    named (and thus configured) current_user.

--- spec/dummy/config/initializers/rollbar.heroku.rb
+++ spec/dummy/config/initializers/rollbar.heroku.rb
@@ -16,7 +16,7 @@ if ENV['HEROKU']
     # By default, Rollbar will try to call the `current_user` controller method
     # to fetch the logged-in user object, and then call that object's `id`,
     # `username`, and `email` methods to fetch those properties. To customize:
-    # config.person_method = "my_current_user"
+    config.person_method = 'the_current_user'
     # config.person_id_method = "my_id"
     # config.person_username_method = "my_username"
     # config.person_email_method = "my_email"
--- spec/dummy/config/initializers/thredded.rb
+++ spec/dummy/config/initializers/thredded.rb
@@ -2,7 +2,7 @@
 Thredded.user_class = 'User'
 Thredded.user_name_column = :name
 Thredded.user_path = ->(user) { main_app.user_path(user.id) }
-Thredded.current_user_method = :"current_#{Thredded.user_class.name.underscore}"
+Thredded.current_user_method = :"the_current_#{Thredded.user_class.name.underscore}"
 Thredded.email_incoming_host = 'incoming.example.com'
 Thredded.email_from = 'no-reply@example.com'
 Thredded.email_outgoing_prefix = '[Thredded] '
