5508c5858c25c7c0295833b0168ce2aed31112eb
Tim Diggins <tim@red56.co.uk>
2016-09-22 09:01:20 +0100

    make reset support only targeted when set

--- spec/support/thredded_reset.rb
+++ spec/support/thredded_reset.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 RSpec.configure do |c|
-  c.around(:example) do |example|
+  c.around(:example, :thredded_reset) do |example|
     # if you specify some variables in thredded_reset we'll reset them after running
     thredded_reset = (example.metadata[:thredded_reset] || []).map do |variable|
       [variable, Thredded.class_variable_get(variable)]
