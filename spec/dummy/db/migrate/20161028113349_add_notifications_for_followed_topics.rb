5fea4cf3c50ad8b225792f757e2844cdc6e3fd0a
EllaNancyFay <ella@red56.uk>
2016-10-28 16:53:43 +0100

    migration to remove old preference fields

--- spec/dummy/db/migrate/20161028113349_add_notifications_for_followed_topics.rb
+++ spec/dummy/db/migrate/20161028113349_add_notifications_for_followed_topics.rb
@@ -4,5 +4,8 @@ class AddNotificationsForFollowedTopics < ActiveRecord::Migration
     add_column :thredded_user_messageboard_preferences, :notifications_for_followed_topics, :string, default: ''
     add_column :thredded_user_preferences, :notifications_for_private_topics, :string, default: ''
     add_column :thredded_user_messageboard_preferences, :notifications_for_private_topics, :string, default: ''
+    remove_column :thredded_user_preferences, :followed_topic_emails
+    remove_column :thredded_user_messageboard_preferences, :followed_topic_emails
+    remove_column :thredded_user_preferences, :notify_on_message
   end
 end
