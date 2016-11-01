b2ceed247c3fcafd8ba6c70428c7a3a7821a22a8
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-11-01 15:22:38 -0700

    Add an index to the demo app users.name

--- spec/dummy/db/migrate/20130430210842_create_users.rb
+++ spec/dummy/db/migrate/20130430210842_create_users.rb
@@ -2,10 +2,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :email
 
       t.timestamps null: false
     end
+    DbTextSearch::CaseInsensitive.add_index connection, :users, :display_name
   end
 end
