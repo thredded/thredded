18ad188b2309cfe5883a0e4754c20acbf9ab2a69
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-11-06 08:37:05 -0800

    fix class name

--- app/commands/thredded/at_notification_extractor.rb
+++ app/commands/thredded/at_notification_extractor.rb
@@ -13,7 +13,7 @@ module Thredded
       # * When parsing, @-mentions within <a> tags will not be considered.
       # * We can't always generate the user URL here because request.host is not available.
       html = @post.filtered_content(view_context, users_provider: nil)
-      HTML::Pipeline::AtMentionFilter.new(
+      Thredded::HtmlPipeline::AtMentionFilter.new(
         html,
         view_context: view_context,
         users_provider: -> (user_names) { @post.readers_from_user_names(user_names).to_a }
