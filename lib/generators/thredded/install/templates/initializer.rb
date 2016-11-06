a2527c903bfceb77b35504f15d9756c424dfb605
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-11-06 11:35:03 -0800

    Split ContentFormatter filters into groups
    Splits filters into 4 groups based on what they process:
    
    * Pre-processing (`before_markup_filters`), input: markup, output: markup.
    * Core markup processing (`markup_filters`). input: markup, output: html.
    * Output post-processing (`after_markup_filters`). input: html, output: html.
    * Sanitization (`sanitization_filters`). input: html, output: sanitized html.
    
    Also these new filter arrays are not frozen.
    Doesn't seem very important to freeze them, and makes customization a bit
    simpler.
    
    This makes registering content formatting plugins easier for plugin authors.

--- lib/generators/thredded/install/templates/initializer.rb
+++ lib/generators/thredded/install/templates/initializer.rb
@@ -76,8 +76,9 @@ Thredded.layout = 'thredded/application'
 # Customize the way Thredded handles post formatting.
 
 # Change the default html-pipeline filters used by thredded.
-# E.g. to remove BBCode support:
-# Thredded::ContentFormatter.pipeline_filters -= [HTML::Pipeline::BbcodeFilter]
+# E.g. to replace default emoji filter with your own:
+# Thredded::ContentFormatter.markup_filters[
+#   Thredded::ContentFormatter.markup_filters.index(HTML::Pipeline::EmojiFilter)] = MyEmojiFilter
 
 # Change the HTML sanitization settings used by Thredded.
 # See the Sanitize docs for more information on the underlying library: https://github.com/rgrove/sanitize/#readme
