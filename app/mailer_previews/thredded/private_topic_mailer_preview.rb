dd510075cb0a8709b79f45927fe41b179e65c82c
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-04 18:41:09 +0000

    Fix PrivateTopicMailerPreview error

--- app/mailer_previews/thredded/private_topic_mailer_preview.rb
+++ app/mailer_previews/thredded/private_topic_mailer_preview.rb
@@ -4,11 +4,9 @@ module Thredded
   class PrivateTopicMailerPreview < BaseMailerPreview
     def message_notification
       PrivateTopicMailer.message_notification(
-        mock_private_topic.tap do |private_topic|
-          private_topic.posts = [
-            mock_private_post(content: mock_content(mention_users: ['glebm']), postable: private_topic)
-          ]
-        end,
+        mock_private_topic(posts: [
+          mock_private_post(content: mock_content(mention_users: ['glebm']))
+        ]),
         %w(glebm@test.com joel@test.com)
       )
     end
