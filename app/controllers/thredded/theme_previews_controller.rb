d92daedfec87198ee0881e229f8b1be13c28bc1d
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-26 14:39:36 -0500

    Test

--- app/controllers/thredded/theme_previews_controller.rb
+++ app/controllers/thredded/theme_previews_controller.rb
@@ -4,11 +4,16 @@ module Thredded
     def show
       @messageboard = Messageboard.first
       fail Thredded::Errors::DatabaseEmpty unless @messageboard
-      @user              = if thredded_current_user.thredded_anonymous?
-                             Thredded.user_class.new(id: 1334, name: 'joe', email: 'joe@example.com')
-                           else
-                             thredded_current_user
-                           end
+
+      @user = if thredded_current_user.thredded_anonymous?
+                Thredded.user_class.new(
+                  id: 1334,
+                  name: 'joe',
+                  email: 'joe@example.com'
+                )
+              else
+                thredded_current_user
+              end
       @messageboards     = Messageboard.all
       @topics            = TopicsPageView.new(@user, @messageboard.topics.page(1).limit(3))
       @private_topics    = PrivateTopicsPageView.new(@user, @user.thredded_private_topics.page(1).limit(3))
@@ -23,7 +28,10 @@ module Thredded
       @private_topic     = PrivateTopicView.from_user(private_topic, @user)
       @private_posts     = TopicPostsPageView.new(@user, private_topic, private_topic.posts.page(1).limit(3))
       @private_post      = private_topic.posts.build(
-        id: 1337, postable: private_topic, content: 'A private hello world', user: @user
+        id: 1337,
+        postable: private_topic,
+        content: 'A private hello world',
+        user: @user
       )
       @preferences = UserPreferencesForm.new(user: @user, messageboard: @messageboard)
     end
