9d459852d4bc85d4732d01153b5cf34858b12546
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-02-12 17:23:31 +0000

    fix cop string literals in notify_following_users_spec.rb

--- spec/commands/thredded/notify_following_users_spec.rb
+++ spec/commands/thredded/notify_following_users_spec.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 require 'spec_helper'
-# rubocop:disable StringLiterals
 
 module Thredded
   describe NotifyFollowingUsers do
     describe '#targeted_users' do
       let(:post) { create(:post, user: poster, postable: topic) }
-      let(:poster) { create(:user, name: "poster") }
-      let!(:follower) { create(:user_topic_follow, user: create(:user, name: "follower"), topic: topic).user }
+      let(:poster) { create(:user, name: 'poster') }
+      let!(:follower) { create(:user_topic_follow, user: create(:user, name: 'follower'), topic: topic).user }
       let(:topic) { create(:topic, messageboard: messageboard) }
       let!(:messageboard) { create(:messageboard) }
       let(:notifier) { EmailNotifier.new }
       subject { NotifyFollowingUsers.new(post).targeted_users(notifier) }
 
-      it "includes followers where preference to receive these notifications" do
+      it 'includes followers where preference to receive these notifications' do
         create(:notifications_for_followed_topics,
                notifier_key: 'email',
                user: follower,
@@ -39,9 +38,9 @@ module Thredded
           expect(subject).not_to include(follower)
         end
 
-        context "with the MockNotifier" do
+        context 'with the MockNotifier' do
           let(:notifier) { MockNotifier.new.resetted }
-          it "does include that user" do
+          it 'does include that user' do
             expect(subject).to include(follower)
           end
         end
@@ -56,14 +55,14 @@ module Thredded
                  enabled: false)
         end
 
-        context "with the EmailNotifier" do
+        context 'with the EmailNotifier' do
           let(:notifier) { EmailNotifier.new }
-          it "does includes that user" do
+          it 'does includes that user' do
             expect(subject).to include(follower)
           end
         end
 
-        context "with the MockNotifier" do
+        context 'with the MockNotifier' do
           let(:notifier) { MockNotifier.new.resetted }
           it "doesn't include that user" do
             expect(subject).not_to include(follower)
@@ -79,14 +78,14 @@ module Thredded
                  enabled: false)
         end
 
-        context "with the EmailNotifier" do
+        context 'with the EmailNotifier' do
           let(:notifier) { EmailNotifier.new }
-          it "does includes that user" do
+          it 'does includes that user' do
             expect(subject).to include(follower)
           end
         end
 
-        context "with the MockNotifier" do
+        context 'with the MockNotifier' do
           let(:notifier) { MockNotifier.new.resetted }
           it "doesn't include that user" do
             expect(subject).not_to include(follower)
@@ -102,17 +101,17 @@ module Thredded
       let(:targeted_users) { [build_stubbed(:user)] }
       before { allow(command).to receive(:targeted_users).and_return(targeted_users) }
 
-      it "sends email" do
+      it 'sends email' do
         expect { command.run }.to change { ActionMailer::Base.deliveries.count }
         # see EmailNotifier spec for more detailed specs
       end
 
-      context "with the MockNotifier", thredded_reset: ["@@notifiers"] do
+      context 'with the MockNotifier', thredded_reset: ['@@notifiers'] do
         before { Thredded.notifiers = [MockNotifier.new.resetted] }
         it "doesn't send any emails" do
           expect { command.run }.not_to change { ActionMailer::Base.deliveries.count }
         end
-        it "uses MockNotifier" do
+        it 'uses MockNotifier' do
           expect { command.run }.to change { MockNotifier.users_notified_of_new_post }
         end
       end
