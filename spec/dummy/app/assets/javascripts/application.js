9de39207e40db8c21f02bb4015fda9ae50414943
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-04-06 00:17:06 +0100

    Demo app: add jquery.timeago locales for all langs

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -2,6 +2,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require thredded/dependencies
+//= require locales/jquery.timeago.es
+//= require locales/jquery.timeago.pl
 //= require locales/jquery.timeago.pt-br
+//= require locales/jquery.timeago.ru
 //= require thredded/thredded
 //= require_tree ./app
