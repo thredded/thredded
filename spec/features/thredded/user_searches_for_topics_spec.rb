9b7ddd9c5fd49c22954e88dbaa88e281c8a3f198
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-04-12 00:41:20 +0100

    Emphasize when a search is in a messageboard
    Also, provide a link to Search Everywhere if there are no results in the
    messageboard.

--- spec/features/thredded/user_searches_for_topics_spec.rb
+++ spec/features/thredded/user_searches_for_topics_spec.rb
@@ -6,30 +6,32 @@ feature 'User searching topics' do
 
   # On MySQL, a transaction has to complete before the full text search index is updated
   before :all do
-    messageboard = create(:messageboard)
-    create(:topic, title: title, messageboard: messageboard)
-    create_list(:topic, 2, messageboard: messageboard)
-    @topics = PageObject::Topics.new(messageboard)
+    @messageboard = create(:messageboard)
+    create(:topic, title: title, messageboard: @messageboard)
+    create_list(:topic, 2, messageboard: @messageboard)
+    @topics = PageObject::Topics.new(@messageboard)
   end
 
   after :all do
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  search_and_expect_found = lambda do
+  scenario 'in a messageboard' do
+    @topics.visit_style_guide
     @topics.search_for(title)
-    expect(page).to have_content(I18n.t('thredded.topics.search.results_message', query: "'#{title}'"))
+    expect(page.source).to(
+      include(I18n.t('thredded.topics.search.results_in_messageboard_message_html',
+                     query: title, messageboard: @messageboard.name))
+    )
     expect(@topics.normal_topics.size).to eq(1)
     expect(@topics).to have_topic_titled(title)
   end
 
-  scenario 'in a messageboard' do
-    @topics.visit_style_guide
-    instance_exec(&search_and_expect_found)
-  end
-
   scenario 'globally' do
     visit thredded.root_path
-    instance_exec(&search_and_expect_found)
+    @topics.search_for(title)
+    expect(page.source).to include(I18n.t('thredded.topics.search.results_message_html', query: title))
+    expect(@topics.normal_topics.size).to eq(1)
+    expect(@topics).to have_topic_titled(title)
   end
 end
