b464f17b88a6e0cdbc0f6a29cfec6b4205dfc6f5
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-07-21 23:16:53 +0100

    Localize mailers

--- app/mailers/thredded/post_mailer.rb
+++ app/mailers/thredded/post_mailer.rb
@@ -3,14 +3,19 @@
 module Thredded
   class PostMailer < Thredded::BaseMailer
     def post_notification(post_id, emails)
-      @post                = find_record Thredded::Post, post_id
-      email_details        = Thredded::TopicEmailView.new(@post.postable)
+      @post = find_record Thredded::Post, post_id
+      email_details = Thredded::TopicEmailView.new(@post.postable)
       headers['X-SMTPAPI'] = email_details.smtp_api_tag('post_notification')
 
-      mail from:     email_details.no_reply,
-           to:       email_details.no_reply,
-           bcc:      emails,
-           subject:  email_details.subject
+      mail from: email_details.no_reply,
+           to: email_details.no_reply,
+           bcc: emails,
+           subject: [
+             Thredded.email_outgoing_prefix,
+             t('thredded.emails.post_notification.subject',
+               user: @post.user.thredded_display_name,
+               topic_title: @post.postable.title)
+           ].compact.join
     end
   end
 end
