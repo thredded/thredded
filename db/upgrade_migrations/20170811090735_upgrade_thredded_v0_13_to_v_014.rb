0bf82088ad036afed686efc9818c1ba03ac2e09d
tiii <gh@tiii.de>
2017-12-06 14:00:24 +0100

    fix index creation in v013_to_v14 update migration

--- db/upgrade_migrations/20170811090735_upgrade_thredded_v0_13_to_v_014.rb
+++ db/upgrade_migrations/20170811090735_upgrade_thredded_v0_13_to_v_014.rb
@@ -8,11 +8,13 @@ class UpgradeThreddedV013ToV014 < Thredded::BaseMigration
     remove_index :thredded_user_details,
                  name: :index_thredded_user_details_on_user_id
     add_index :thredded_user_details,
+              :user_id,
               name: :index_thredded_user_details_on_user_id,
               unique: true
     remove_index :thredded_user_preferences,
                  name: :index_thredded_user_preferences_on_user_id
     add_index :thredded_user_preferences,
+              :user_id,
               name: :index_thredded_user_preferences_on_user_id,
               unique: true
   end
