581db44c29f170e9a9cf0b633879f45ca06c0160
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-02-11 18:56:29 +0000

    Extract user -> autocomplete result conversion
    to make customization easier

--- app/controllers/thredded/autocomplete_users_controller.rb
+++ app/controllers/thredded/autocomplete_users_controller.rb
@@ -8,12 +8,18 @@ module Thredded
       authorize_creating Thredded::PrivateTopicForm.new(user: thredded_current_user).private_topic
       users = params.key?(:q) ? users_by_prefix : users_by_ids
       render json: {
-        results: users.map do |user|
-          { id: user.id,
-            name: user.send(Thredded.user_name_column),
-            display_name: user.send(Thredded.user_display_name_method),
-            avatar_url: Thredded.avatar_url.call(user) }
-        end
+        results: users.map { |user| user_to_autocomplete_result(user) }
+      }
+    end
+
+    protected
+
+    def user_to_autocomplete_result(user)
+      {
+        id: user.id,
+        name: user.send(Thredded.user_name_column),
+        display_name: user.send(Thredded.user_display_name_method),
+        avatar_url: Thredded.avatar_url.call(user)
       }
     end
 
