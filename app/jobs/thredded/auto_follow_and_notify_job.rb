a7024020b1a0d4f82721f9958fd4295c4aed5294
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-06-06 19:47:01 +0100

    Skip AutoFollowAndNotifyJob if the post is gone

--- app/jobs/thredded/auto_follow_and_notify_job.rb
+++ app/jobs/thredded/auto_follow_and_notify_job.rb
@@ -5,7 +5,8 @@ module Thredded
     queue_as :default
 
     def perform(post_id)
-      post = Thredded::Post.find(post_id)
+      post = Thredded::Post.find_by(id: post_id)
+      return if post.nil? || post.postable.nil?
 
       Thredded::AutofollowUsers.new(post).run
       Thredded::NotifyFollowingUsers.new(post).run
