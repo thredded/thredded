a572f9d4402b07f01f05e87f782c10e1ab1fa82d
Tim Diggins <tim@red56.uk>
2018-10-12 16:22:26 +0100

    remove antique reference to messageboard from private topic spec.

--- spec/features/thredded/user_sends_new_private_topic_spec.rb
+++ spec/features/thredded/user_sends_new_private_topic_spec.rb
@@ -5,8 +5,7 @@ require 'spec_helper'
 RSpec.feature 'User sends a new private topic' do
   it 'with title, recipient and content' do
     PageObject::User.new(create(:user, name: 'joel')).log_in
-    messageboard = create(:messageboard)
-    private_topic = PageObject::PrivateTopics.new(messageboard)
+    private_topic = PageObject::PrivateTopics.new('A new message')
     private_topic.create_private_topic
     private_topic.visit_private_topic_list
     expect(private_topic).to be_on_private_list
