69c743670f3abcf28bf489e0d300a36061938ad4
Tim Diggins <tim@red56.uk>
2018-10-13 18:47:45 +0100

    Add follow icon to unread topics count. (#751)

---
+++ app/helpers/thredded/icon_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Thredded
+  module IconHelper
+    def shared_inline_svg(filename, **args)
+      svg = content_tag :svg, **args do
+        content_tag :use, '', 'xlink:href' => "##{thredded_icon_id(filename)}"
+      end
+      if (definition = define_svg_icons(filename))
+        definition + svg
+      else
+        svg
+      end
+    end
+
+    def define_svg_icons(*filenames)
+      return if filenames.blank?
+      sb = filenames.map do |filename|
+        inline_svg_once(filename, id: thredded_icon_id(filename))
+      end
+      return if sb.compact.blank?
+      content_tag :div, safe_join(sb), class: 'thredded--svg-definitions'
+    end
+
+    def inline_svg_once(filename, id:, **transform_params)
+      return if @already_inlined_svg_ids&.include?(id)
+      record_already_inlined_svg(filename, id)
+      inline_svg(filename, id: id, **transform_params)
+    end
+
+    private
+
+    def record_already_inlined_svg(filename, id)
+      if filename.is_a?(String) # in case it's an IO or other
+        fail "Please use id: #{thredded_icon_id(filename)}" unless id == thredded_icon_id(filename)
+      end
+      @already_inlined_svg_ids ||= []
+      @already_inlined_svg_ids << id
+    end
+
+    def thredded_icon_id(svg_filename)
+      "thredded-#{File.basename(svg_filename, '.svg').dasherize}-icon"
+    end
+  end
+end
---
+++ spec/helpers/thredded/icon_helper_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Thredded
+  describe IconHelper do
+    include IconHelper
+    # rubocop:disable Rails/OutputSafety
+    let(:settings_svg) { '<svg id="thredded-settings-icon">settings</svg>'.html_safe }
+    let(:follow_svg) { '<svg id="thredded-follow-icon">follow</svg>'.html_safe }
+    # rubocop:enable Rails/OutputSafety
+
+    describe '#inline_svg_once' do
+      it 'calls inline_svg' do
+        expect(self).to receive(:inline_svg).and_return settings_svg
+        expect(inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon'))
+          .to eq(settings_svg)
+      end
+
+      it 'only inlines once' do
+        expect(self).to receive(:inline_svg).once.and_return(settings_svg)
+        expect(inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon')).to eq(settings_svg)
+        expect(inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon')).to be_nil
+      end
+
+      it 'requires a specific id based on filename' do
+        expect { inline_svg_once('thredded/much_ado_about_whatever.svg', id: 'whatever-i-feel-like') }
+          .to raise_error(/thredded-much-ado-about-whatever-icon/)
+      end
+    end
+
+    describe '#define_svg_icons' do
+      it 'works for no icons' do
+        expect(define_svg_icons).to be_nil
+      end
+      context 'with one icon' do
+        subject(:html) { define_svg_icons('thredded/settings.svg') }
+
+        it 'wraps with definitions' do
+          expect(self).to receive(:inline_svg).with('thredded/settings.svg', any_args).and_return settings_svg
+          expect(html).to start_with('<div class="thredded--svg-definitions">')
+          expect(html).to end_with('</div>')
+          expect(html).to include(settings_svg)
+        end
+        context 'when already included' do
+          before do
+            allow(self).to receive(:inline_svg).and_return(settings_svg)
+            inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon')
+          end
+
+          it 'is nil' do
+            expect(html).to be_nil
+          end
+        end
+      end
+
+      context 'with two icons' do
+        subject(:html) { define_svg_icons('thredded/settings.svg', 'thredded/follow.svg') }
+
+        it 'wraps with definitions' do
+          expect(self).to receive(:inline_svg).with('thredded/settings.svg', any_args).and_return settings_svg
+          expect(self).to receive(:inline_svg).with('thredded/follow.svg', any_args).and_return follow_svg
+          expect(html).to start_with('<div class="thredded--svg-definitions">')
+          expect(html).to end_with('</div>')
+          expect(html).to include(settings_svg)
+          expect(html).to include(follow_svg)
+        end
+        context 'when one already included' do
+          before do
+            allow(self).to receive(:inline_svg).once.and_return(settings_svg)
+            inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon')
+          end
+
+          it 'has the other one' do
+            expect(self).to receive(:inline_svg).with('thredded/follow.svg', any_args).and_return follow_svg
+            expect(html).not_to include(settings_svg)
+            expect(html).to include(follow_svg)
+          end
+        end
+
+        context 'when both already included' do
+          before do
+            allow(self).to receive(:inline_svg).and_return(settings_svg)
+            inline_svg_once('thredded/settings.svg', id: 'thredded-settings-icon')
+            inline_svg_once('thredded/follow.svg', id: 'thredded-follow-icon')
+          end
+
+          it 'is nil' do
+            expect(html).to be_nil
+          end
+        end
+      end
+    end
+
+    describe '#shared_inline_svg' do
+      before do
+        allow(self).to receive(:inline_svg).and_return(settings_svg)
+      end
+
+      it 'outputs definition' do
+        html = shared_inline_svg('thredded/settings.svg')
+        expect(html).to include('<div class="thredded--svg-definitions">')
+        expect(html).to include('id="thredded-settings-icon"')
+        expect(html).to include(settings_svg)
+      end
+      it 'outputs use' do
+        expect(shared_inline_svg('thredded/settings.svg'))
+          .to include('<svg><use xlink:href="#thredded-settings-icon"></use></svg>')
+      end
+      it 'passes through args' do
+        html = shared_inline_svg('thredded/settings.svg', class: 'flong')
+        expect(html).to include('<svg class="flong">')
+      end
+    end
+  end
+end
