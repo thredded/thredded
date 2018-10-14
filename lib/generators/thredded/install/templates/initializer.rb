c3ef7ae11d2d9b97a4d24f935e5bb220e50cbec3
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-10-14 23:09:22 +0100

    Initializer: Improved error handling example

--- lib/generators/thredded/install/templates/initializer.rb
+++ lib/generators/thredded/install/templates/initializer.rb
@@ -128,9 +128,15 @@ Thredded.layout = 'thredded/application'
 #
 # Rails.application.config.to_prepare do
 #   Thredded::ApplicationController.module_eval do
+#     # Render sign in page:
 #     rescue_from Thredded::Errors::LoginRequired do |exception|
-#       @message = exception.message
-#       render template: 'sessions/new', status: :forbidden
+#       flash.now[:notice] = exception.message
+#       controller = Users::SessionsController.new
+#       controller.request = request
+#       controller.request.env['devise.mapping'] = Devise.mappings[:user]
+#       controller.response = response
+#       controller.response_options = { status: :forbidden }
+#       controller.process(:new)
 #     end
 #   end
 # end
