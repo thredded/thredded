1c7cc59a501cfe687b4df30b426b841dbfae99d4
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-12-03 01:13:29 +0000

    Bump to v0.16.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.16.3
+
+Fixes private topic form preview (regression in v0.16.2).
+
 # v0.16.2
 
 ## Added
--- lib/thredded/version.rb
+++ lib/thredded/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Thredded
-  VERSION = '0.16.2'
+  VERSION = '0.16.3'
 end
