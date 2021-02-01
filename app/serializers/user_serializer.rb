01f5091605c01569ea3331de43a6d6128598b282
fhs42553 <ckoch.mmt-b2018@fh-salzburg.ac.at>
2021-02-01 19:26:21 +0100

    Add User Index EP BRICK-108, EP for User Details BRICK-70, add Attributes from User Details BRICK-75

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,11 @@
 
 class UserSerializer
   include JSONAPI::Serializer
+
+  attributes  :admin, :display_name, :created_at, :updated_at
+  attribute :avatar do |user|
+    Rails.application.routes.url_helpers.rails_blob_url(user.avatar, only_path: true) if user.avatar.attached?
+  end
+
+  has_one :thredded_user_detail
 end
