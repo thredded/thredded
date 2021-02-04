b34795d4c6bd1696ba94e3959970a4529a6d2eca
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2021-02-04 17:36:16 +0100

    Add movies_count to user_details via counter_cache (https://stom.atlassian.net/browse/BRICK-135)

--- app/models/thredded/topic_movie.rb
+++ app/models/thredded/topic_movie.rb
@@ -2,5 +2,11 @@
 
 module Thredded
   class TopicMovie < Thredded::Topic
+    belongs_to :user_detail,
+               primary_key:   :user_id,
+               foreign_key:   :user_id,
+               inverse_of:    :topics,
+               counter_cache: :movies_count,
+               **(Thredded.rails_gte_51? ? { optional: true } : {})
   end
 end
