f9b26d135dbedd35b2bb711a59ec71a922c87425
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2021-05-04 12:16:49 +0200

    Include BrowserNotification in SessionsController (https://stom.atlassian.net/browse/BRICK-239)

--- spec/controllers/thredded/sessions_controller_spec.rb
+++ spec/controllers/thredded/sessions_controller_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe Thredded::SessionsController do
   let(:user_blocked) { create(:user, :blocked) }
 
   describe 'GET logged_in_user' do
-    it 'returns 204 for logged in user that is approved' do
+    it 'returns 200 for logged in user that is approved' do
       allow(controller).to receive_messages(the_current_user: user_approved)
       get :logged_in_user
-      expect(response).to have_http_status(204)
+      expect(response).to have_http_status(200)
     end
 
     it 'returns 401 for logged in user that is blocked' do
