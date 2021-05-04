2bfb9737fa43abc283551b3d1c3759ed36638a95
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2021-05-04 12:08:49 +0200

    Add BrowserNotifier (https://stom.atlassian.net/browse/BRICK-239)

---
+++ app/notifiers/thredded/browser_notifier.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Thredded
+  class BrowserNotifier
+    def human_name
+      'als Browser-Benachrichtigung'
+    end
+
+    def key
+      'browser'
+    end
+
+    def new_post(post, users)
+      users.map do | user |
+        Thredded::Notification.new(
+          user: user,
+          name: "#{ post.user.thredded_display_name } kommentierte #{ post.postable.title }",
+          url: "#{ Rails.configuration.frontend_url }forum/#{ post.messageboard.slug }/#{ post.postable.id }"
+        ).save!
+      end
+    end
+
+    def new_private_post(post, users)
+      #  not implemented in frontend yet
+    end
+
+    def updated_moderation_state(moderation_state, user_detail)
+      Thredded::Notification.new(
+        user: user_detail.user,
+        name: moderation_state == "blocked" ? "Du wurdest von einem Administrator gesperrt" : "Dein Konto wurde soeben freigeschalten"
+      ).save!
+    end
+
+    def new_relaunch_user(relaunch_user)
+      # ignore
+    end
+  end
+end
