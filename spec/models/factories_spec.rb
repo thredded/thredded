7fc31eb3df4aef2ce9e892bdd2ddb8012f454f85
Tim Diggins <tim@red56.uk>
2021-10-20 15:56:22 +0100

    allow factories spec to be run singly

--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'factories'
 
 RSpec.describe 'Factory' do # rubocop:disable RSpec/DescribeClass
   FactoryBot.factories.map(&:name).each do |factory_name|
