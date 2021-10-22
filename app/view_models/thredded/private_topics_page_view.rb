155238401d25826f85eb95cdb5da5e7a4fef5e31
Tim Diggins <tim@red56.uk>
2021-10-22 15:49:34 +0100

    rename instance variable to make delegation clearer (#905)

--- app/view_models/thredded/private_topics_page_view.rb
+++ app/view_models/thredded/private_topics_page_view.rb
@@ -15,13 +15,13 @@ module Thredded
     delegate :total_pages,
              :current_page,
              :limit_value,
-             to: :@topics_page_scope
+             to: :@topics_paginator
 
     # @param user [Thredded.user_class] the user who is viewing the private topics page
     # @param topics_page_scope [ActiveRecord::Relation<Thredded::PrivateTopic>] a kaminari-decorated ".page" scope
     def initialize(user, topics_page_scope)
-      @topics_page_scope = refine_scope(topics_page_scope)
-      @topic_views = @topics_page_scope.with_read_states(user).map do |(topic, read_state)|
+      @topics_paginator = refine_scope(topics_page_scope)
+      @topic_views = @topics_paginator.with_read_states(user).map do |(topic, read_state)|
         Thredded::PrivateTopicView.new(topic, read_state, Pundit.policy!(user, topic))
       end
     end
