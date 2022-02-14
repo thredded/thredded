3a441df6a2e10643db34a1c8deabcd98e32472fd
Tim Diggins <tim@red56.uk>
2022-02-14 08:38:11 +0000

    add comment [CI SKIP]

--- spec/features/thredded/user_views_a_topic_with_super_rich_content_spec.rb
+++ spec/features/thredded/user_views_a_topic_with_super_rich_content_spec.rb
@@ -7,6 +7,8 @@ RSpec.feature 'View a topic with super-rich content (smoke test)', type: :featur
   let!(:post) { create(:post, content: FakeContent.post_content(with_everything: true)) }
   let(:user) { create(:user) }
 
+  # This takes a looong time and you may need to increase the timeout for Ferrum (the browser automation under cuprite)
+  # You can do this with setting env FERRUM_DEFAULT_TIMEOUT=20 before running this tests, see also .travis.yml:98
   it 'can view a topic with very rich content (smoke test)', js: true do
     topic.visit_topic
   end
