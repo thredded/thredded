6c0047c4aead77ce6f25ef2bf2818c34f90ad53f
Tim Diggins <tim@red56.uk>
2024-11-07 17:58:51 +0000

    always raise exceptions in tests to make debugging easier

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -7,9 +7,11 @@ Dummy::Application.configure do
 
   config.eager_load = false
   config.cache_classes = true
-  config.consider_all_requests_local = true
+
   config.action_controller.perform_caching = false
-  config.action_dispatch.show_exceptions = false
+  # Raise exceptions instead of rendering exception templates.
+  # using :rescuable swallows some useful debugging info for some kinds of spec errors
+  config.action_dispatch.show_exceptions = :none
   config.action_controller.allow_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :test
